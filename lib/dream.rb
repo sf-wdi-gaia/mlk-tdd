43f5047ef50962f40d13c44e749269b740aec50e
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-10-11 21:01:44 -0700

    specs Nation & State

--- lib/dream.rb
+++ lib/dream.rb
@@ -1,5 +1,51 @@
+class Nation
+
+  def self.join_hands(white_children, black_children)
+    true
+  end
+
+  def creed
+    "all men are created equal"
+  end
+
+  def rise_up?
+    true
+  end
+
+  def judge_by(attribute)
+    return false if attribute == :color
+    true
+  end
+
+end
+
 class State
+  attr_accessor :name, :people, :status
+
+  def initialize(name)
+    @name = name
+    @people = [{ancestors: "slaves"}, {ancestors: "slave-owners"}]
+    @people = [{age: Random.new.rand(18), color: "black"}, {age: Random.new.rand(18), color: "white"}] if @name == "Alabama"
+    @status = "a state sweltering with the heat of injustice and oppression" if @name == "Mississippi"
+  end
+
+  def table_of_brotherhood
+    @people.select { |person| person[:ancestors] != "slave-owners" }
+  end
+
+  def sit_at_table(people)
+    true
+  end
+
+  def emancipate
+    @status = "an oasis of freedom and justice"
+  end
+
 end
 
 class Freedom
+
+  def ring
+  end
+
 end
